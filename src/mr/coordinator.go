1579ca4c188f421e676281249c4730b7c1b331d2
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2023-04-02 17:38:40 +0200

    Reformat add some variables to coordinator

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,39 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
+type WorkerId int
+type TaskId int
+
+/*
+fetchTask()
+*/
 
 type Coordinator struct {
 	// Your definitions here.
-
+	assignment                      map[WorkerId][]TaskId
+	nMap, nReduce                   int
+	nMapCompleted, nReduceCompleted int
+	nWorkers, nextWorkerId          int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +47,25 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
+	ret := true
 
 	// Your code here.
-
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
+// each file corresponds to one "split",
+// and is the input to one Map task.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
